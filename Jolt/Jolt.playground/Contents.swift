3d20d7d288dfbc4ace6444f370d8d2994b381c1c
Tyler Cloutier <cloutiertyler@aol.com>
2016-01-24 21:16:34 -0800

    Sharing scheme and added playground.

--- Jolt/Jolt.playground/Contents.swift
+++ Jolt/Jolt.playground/Contents.swift
@@ -2,5 +2,12 @@
 
 import Jolt
 
+let numbers: [Double] = [1.4, 2.6, 3.0, 5.5, 6.5]
+var sum: Double = 0.0
 
-[1, 2, 4.0].sum()
+// Naïve Swift Implementation
+sum = numbers.reduce(0.0, combine: +)
+
+// Jolt Implementations
+sum = Jolt.sum(numbers)
+sum = numbers.sum()
